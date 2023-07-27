050c9706243af26c8a95a8c63bb28bb2062ba23c
Ben-Peak-Student <BPEAK9465@my.sullivan.edu>
2023-07-27 12:35:03 -0400

    Done

--- Exponents/Form1.Designer.cs
+++ Exponents/Form1.Designer.cs
@@ -28,28 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.xInputTextBox = new System.Windows.Forms.TextBox();
-            this.xInputLabel = new System.Windows.Forms.Label();
+            this.UxInputTextBox = new System.Windows.Forms.TextBox();
+            this.UxInputLabel = new System.Windows.Forms.Label();
             this.xOutputLabel = new System.Windows.Forms.Label();
-            this.xGoButton = new System.Windows.Forms.Button();
-            this.xResetButton = new System.Windows.Forms.Button();
+            this.UxGoButton = new System.Windows.Forms.Button();
+            this.UxResetButton = new System.Windows.Forms.Button();
+            this.UxOutPutTextBox = new System.Windows.Forms.TextBox();
+            this.UxOutPutcube = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // xInputTextBox
+            // UxInputTextBox
             // 
-            this.xInputTextBox.Location = new System.Drawing.Point(92, 45);
-            this.xInputTextBox.Name = "xInputTextBox";
-            this.xInputTextBox.Size = new System.Drawing.Size(100, 20);
-            this.xInputTextBox.TabIndex = 0;
+            this.UxInputTextBox.Location = new System.Drawing.Point(89, 43);
+            this.UxInputTextBox.Name = "UxInputTextBox";
+            this.UxInputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UxInputTextBox.TabIndex = 0;
             // 
-            // xInputLabel
+            // UxInputLabel
             // 
-            this.xInputLabel.AutoSize = true;
-            this.xInputLabel.Location = new System.Drawing.Point(92, 27);
-            this.xInputLabel.Name = "xInputLabel";
-            this.xInputLabel.Size = new System.Drawing.Size(85, 13);
-            this.xInputLabel.TabIndex = 3;
-            this.xInputLabel.Text = "Enter an integer:";
+            this.UxInputLabel.AutoSize = true;
+            this.UxInputLabel.Location = new System.Drawing.Point(95, 27);
+            this.UxInputLabel.Name = "UxInputLabel";
+            this.UxInputLabel.Size = new System.Drawing.Size(85, 13);
+            this.UxInputLabel.TabIndex = 3;
+            this.UxInputLabel.Text = "Enter an integer:";
             // 
             // xOutputLabel
             // 
@@ -60,36 +62,54 @@
             this.xOutputLabel.TabIndex = 4;
             this.xOutputLabel.Visible = false;
             // 
-            // xGoButton
+            // UxGoButton
             // 
-            this.xGoButton.Location = new System.Drawing.Point(105, 96);
-            this.xGoButton.Name = "xGoButton";
-            this.xGoButton.Size = new System.Drawing.Size(75, 67);
-            this.xGoButton.TabIndex = 6;
-            this.xGoButton.Text = "Go!";
-            this.xGoButton.UseVisualStyleBackColor = true;
-            this.xGoButton.Click += new System.EventHandler(this.xGoButton_Click);
+            this.UxGoButton.Location = new System.Drawing.Point(98, 69);
+            this.UxGoButton.Name = "UxGoButton";
+            this.UxGoButton.Size = new System.Drawing.Size(75, 67);
+            this.UxGoButton.TabIndex = 6;
+            this.UxGoButton.Text = "Go!";
+            this.UxGoButton.UseVisualStyleBackColor = true;
+            this.UxGoButton.Click += new System.EventHandler(this.xGoButton_Click);
             // 
-            // xResetButton
+            // UxResetButton
             // 
-            this.xResetButton.Location = new System.Drawing.Point(105, 212);
-            this.xResetButton.Name = "xResetButton";
-            this.xResetButton.Size = new System.Drawing.Size(75, 23);
-            this.xResetButton.TabIndex = 7;
-            this.xResetButton.Text = "Reset";
-            this.xResetButton.UseVisualStyleBackColor = true;
-            this.xResetButton.Click += new System.EventHandler(this.xResetButton_Click);
+            this.UxResetButton.Location = new System.Drawing.Point(98, 197);
+            this.UxResetButton.Name = "UxResetButton";
+            this.UxResetButton.Size = new System.Drawing.Size(75, 23);
+            this.UxResetButton.TabIndex = 7;
+            this.UxResetButton.Text = "Reset";
+            this.UxResetButton.UseVisualStyleBackColor = true;
+            this.UxResetButton.Click += new System.EventHandler(this.xResetButton_Click);
+            // 
+            // UxOutPutTextBox
+            // 
+            this.UxOutPutTextBox.Location = new System.Drawing.Point(89, 142);
+            this.UxOutPutTextBox.Name = "UxOutPutTextBox";
+            this.UxOutPutTextBox.Size = new System.Drawing.Size(90, 20);
+            this.UxOutPutTextBox.TabIndex = 8;
+            this.UxOutPutTextBox.Visible = false;
+            // 
+            // UxOutPutcube
+            // 
+            this.UxOutPutcube.Location = new System.Drawing.Point(89, 168);
+            this.UxOutPutcube.Name = "UxOutPutcube";
+            this.UxOutPutcube.Size = new System.Drawing.Size(91, 20);
+            this.UxOutPutcube.TabIndex = 9;
+            this.UxOutPutcube.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.xResetButton);
-            this.Controls.Add(this.xGoButton);
+            this.Controls.Add(this.UxOutPutcube);
+            this.Controls.Add(this.UxOutPutTextBox);
+            this.Controls.Add(this.UxResetButton);
+            this.Controls.Add(this.UxGoButton);
             this.Controls.Add(this.xOutputLabel);
-            this.Controls.Add(this.xInputLabel);
-            this.Controls.Add(this.xInputTextBox);
+            this.Controls.Add(this.UxInputLabel);
+            this.Controls.Add(this.UxInputTextBox);
             this.Name = "Form1";
             this.Text = "Exponents Demo";
             this.ResumeLayout(false);
@@ -99,11 +119,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox xInputTextBox;
-        private System.Windows.Forms.Label xInputLabel;
+        private System.Windows.Forms.TextBox UxInputTextBox;
+        private System.Windows.Forms.Label UxInputLabel;
         private System.Windows.Forms.Label xOutputLabel;
-        private System.Windows.Forms.Button xGoButton;
-        private System.Windows.Forms.Button xResetButton;
+        private System.Windows.Forms.Button UxGoButton;
+        private System.Windows.Forms.Button UxResetButton;
+        private System.Windows.Forms.TextBox UxOutPutTextBox;
+        private System.Windows.Forms.TextBox UxOutPutcube;
     }
 }
 
--- Exponents/Form1.cs
+++ Exponents/Form1.cs
@@ -8,8 +8,8 @@ using System.Text;
 using System.Windows.Forms;
 
 /**************************************
- * Name:
- * Date:
+ * Name: Ben Peak
+ * Date: 7/27/23
  * Description: A program using user-created method
  *              for the math function Square and Cube
  * Assistance:
@@ -19,6 +19,8 @@ namespace Exponents
 {
     public partial class Form1 : Form
     {
+        public object UxOutPutCube { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
@@ -27,38 +29,56 @@ namespace Exponents
         private void xGoButton_Click(object sender, EventArgs e)
         {
             // STORAGE
-
+            int number;
+            int result;
             // INPUT
-
+            if (!int.TryParse(UxInputTextBox.Text, out number))
+            {
+                
+            }
             // PROCESS
+            result = Square(number);
+            UxOutPutTextBox.Text = "Square: " + result;
             /* calculate the square of the number and add it to the output - 
              * do this with a method call that allows you to pass the number
              * and then receive the answer */
 
             /* calculate the cube of the number and add it to the output - 
              * do this by creating a methd as in the squaring example above */
-
+            result = Cube(number);
+            UxOutPutcube.Text = "Cube: " + result;
             // OUTPUT
             // set the output to visible
-
+            UxOutPutTextBox.Visible = true;
+            UxOutPutcube.Visible = true;
             // turn the Go button off
+            UxGoButton.Visible = false;
         }
 
         public int Square(int num)
         {
             // we will create this code in class
+            return num * num;
         }
 
         public int Cube(int num)
         {
             // we will create this code in class
+            return num * num * num;
         }
 
         private void xResetButton_Click(object sender, EventArgs e)
         {
+
             // the input and output text should be emptied
+            UxInputTextBox.Text = "";
+            UxOutPutTextBox.Text = "";
+            UxOutPutcube.Text = "";
             // turn the output to invisible
+            UxOutPutTextBox.Visible = false;
+            UxOutPutcube.Visible = false;
             // turn the Go button back on
+            UxGoButton.Visible = true;
         }
 
     }
